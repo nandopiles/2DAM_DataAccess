974c58de831e966256ee4a9911c61cfda5a1d3d5
Nando PL <118541862+nandopiles@users.noreply.github.com>
2022-11-23 12:47:43 +0100

    Actualización Act4_2

--- 2T/Act4_2/src/code/Main.java
+++ 2T/Act4_2/src/code/Main.java
@@ -6,8 +6,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import pojos.Departments;
 import exceptions.DepartmentNotFoundException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import exceptions.TeacherNotFoundException;
+import java.util.HashSet;
+import java.util.Set;
+import org.hibernate.Transaction;
+import pojos.Teachers;
 
 /**
  *
@@ -17,8 +20,14 @@ public class Main {
 
     static SessionFactory sesionf = NewHibernateUtil.getSessionFactory();
     static Session sesion = sesionf.openSession();
+    static Transaction tx = null;
     static Scanner eb = new Scanner(System.in);
     static Scanner fr = new Scanner(System.in);
+
+    public static void closeSesions() {
+        sesion.close();
+        sesionf.close();
+    }
 
     public static int menu() {
         int option = 0;
@@ -41,7 +50,7 @@ public class Main {
                     System.out.print("-> ");
                     option = fr.nextInt();
                     repeat = false;
-                    if (option < 1 || option > 8) {
+                    if (option < 1 || option > 11) {
                         System.out.println("\t(-) Option Out of Range");
                         repeat = true;
                     }
@@ -79,21 +88,87 @@ public class Main {
         if (dep == null) {
             throw new DepartmentNotFoundException();
         } else {
-            System.out.println("\tINFO DEPARTMENT(" + id + ")\n=============================");
+            System.out.println("\tINFO DEPARTMENT(" + id + ")\n===================================");
             System.out.printf("Name: %s\n", dep.getName());
             System.out.printf("Office: %s\n", dep.getOffice());
-            System.out.printf("Teachers: %d\n=============================\n", dep.getTeacherses().size());
+            System.out.printf("Teachers: %d\n===================================\n", dep.getTeacherses().size());
+        }
+    }
+
+    public static void infoTeacher(Teachers teacher) {
+        System.out.printf("Name: %s %s\n", teacher.getName(), teacher.getSurname());
+        System.out.printf("Department: %s\n", teacher.getDepartments().getName());
+        System.out.printf("Email: %s\n", teacher.getEmail());
+        System.out.printf("Salary: %d\n", teacher.getSalary());
+        System.out.printf("Start Date: %s\n===================================\n", String.valueOf(teacher.getStartDate()));
+    }
+
+    public static void showTeacher(int id) throws TeacherNotFoundException {
+        Teachers teacher = null;
+        teacher = (Teachers) sesion.get(Teachers.class, id);
+        if (teacher == null) {
+            throw new TeacherNotFoundException();
+        } else {
+            System.out.println("\tINFO TEACHER(" + id + ")\n===================================");
+            infoTeacher(teacher);
+        }
+    }
+
+    public static void showTeachersDept(int id) throws DepartmentNotFoundException {
+        Departments dep = null;
+        dep = (Departments) sesion.get(Departments.class, id);
+        if (dep == null) {
+            throw new DepartmentNotFoundException();
+        } else {
+            Set<Teachers> listT = dep.getTeacherses();
+            System.out.println("\tTEACHERS OF DEPT(" + id + ")\n===================================");
+            for (Teachers teacher : listT) {
+                infoTeacher(teacher);
+            }
+        }
+    }
+
+    public static void createNewDepartment() {
+        int deptNum = 0;
+        String name, office;
+        boolean repeat = true;
+        Set<Teachers> teachers = new HashSet();
+        Departments dep = null;
+
+        System.out.println("\tCREATE DEPARTMENT\n===================================");
+        do {
+            System.out.print("DeptNum: ");
+            try {
+                deptNum = fr.nextInt();
+                repeat = false;
+            } catch (InputMismatchException ex) {
+                System.out.println("(-) Input must be a number");
+                fr.nextLine();
+            }
+        } while (repeat);
+        System.out.print("Name: ");
+        name = eb.nextLine();
+        office = eb.nextLine();
+        dep = new Departments(deptNum, name, office, teachers);
+        sesion.save(dep);
+        tx = sesion.beginTransaction();
+        tx.commit();
+
+        try {
+            showDepartment(deptNum);
+        } catch (DepartmentNotFoundException ex) {
+            System.out.println("\t(-) Department Not Found...\n");
         }
     }
 
     public static void main(String[] args) {
-        int option, idDept;
+        int option;
 
         do {
             option = menu();
             switch (option) {
                 case 1:
-                    idDept = inputId();
+                    int idDept = inputId();
                     try {
                         showDepartment(idDept);
                     } catch (DepartmentNotFoundException ex) {
@@ -101,11 +176,23 @@ public class Main {
                     }
                     break;
                 case 2:
-                    
+                    int idTeacher = inputId();
+                    try {
+                        showTeacher(idTeacher);
+                    } catch (TeacherNotFoundException ex) {
+                        System.out.println("\t(-) Teacher Not Found...\n");
+                    }
                     break;
                 case 3:
+                    idTeacher = inputId();
+                    try {
+                        showTeachersDept(idTeacher);
+                    } catch (DepartmentNotFoundException ex) {
+                        System.out.println("\t(-) Department Not Found...\n");
+                    }
                     break;
                 case 4:
+                    createNewDepartment();
                     break;
                 case 5:
                     break;
@@ -120,9 +207,10 @@ public class Main {
                 case 10:
                     break;
                 case 11:
-                    System.out.println("(+) SEE U!");
                     break;
             }
         } while (option != 11);
+        closeSesions();
+        System.out.println("(+) SEE U!");
     }
 }
---
+++ 2T/Act4_2/src/exceptions/TeacherNotFoundException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+/**
+ *
+ * @author 7J
+ */
+public class TeacherNotFoundException extends Exception {
+
+    public TeacherNotFoundException() {
+    }
+
+    public TeacherNotFoundException(String msg) {
+        super(msg);
+    }
+}
